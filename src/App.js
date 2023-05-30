cdc51569b6aa929a181221b7d0f6a5a80cdf0d72
Aishwarya17m <aishwarya17199@gmail.com>
2023-05-30 17:40:33 +0530

    "add"

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+import HomePage from './HomePage';
+import AdminHome from './Admin/AdminHome';
+import UserHome from './User/UserHome';
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Routes>
+      <Route exact path="/" element={<HomePage/>}></Route>
+      <Route exact path="/AdminHome" element={<AdminHome/>}></Route>
+      <Route exact path="/UserHome" element={<UserHome/>}></Route>
+     </Routes>
     </div>
+    </Router>
+   
   );
 }
 
---
+++ src/User/UserHome.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function UserHome() {
+    
+  return (
+    <div>UserHome</div>
+  )
+}
+
+export default UserHome
