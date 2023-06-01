4a54065394fc8c7d5bb0017fcbd673b310495919
Aishwarya17m <aishwarya17199@gmail.com>
2023-06-01 13:23:03 +0530

    commit changes "1/6/2023"

---
+++ src/Admin/DisplayMovies.js
@@ -0,0 +1,47 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+
+function DisplayMovies() {
+    const [movies,setmovies]=useState([]);
+    useEffect(()=>{
+        axios.get("http://localhost:8282/user/getMovies").then((res)=>{
+            setmovies(res.data)
+
+        })
+    })
+  return (
+    <div>DisplayMovies
+        <table border="2">
+              <tbody>
+                <tr>
+                        <th>Movie Name</th>
+                        <th>Language</th>
+                        <th>Category</th>
+                        <th>Release Date</th>
+                        </tr>
+                    
+        {
+            
+            movies.map(m=>{
+                return(
+               
+                    <tr key={m.movieId}>
+                    <td >{m.movieName}</td>
+                    <td >{m.language}</td>
+                    <td >{m.category}</td>
+                    <td >{m.releaseDate}</td>
+                    </tr>
+
+                  
+                  
+                )
+            })
+         
+        }
+        </tbody>
+           </table>
+    </div>
+  )
+}
+
+export default DisplayMovies
