cb7ab480f811d1ecd84ff4847c305a15c9a9fffd
Aishwarya S <65033336+Aishwarya17m@users.noreply.github.com>
2023-06-05 13:30:43 +0530

    Update AdminHome.js

--- src/Admin/AdminHome.js
+++ src/Admin/AdminHome.js
@@ -3,18 +3,26 @@ import AdminNavbar from './AdminNavbar'
 import AddMovies from './AddMovies';
 import './Styles/AdminHome.css'
 import DisplayMovies from './DisplayMovies';
+import Dashboard from './Dashboard';
 function AdminHome() {
   const [addmovies,setaddmovies]=useState(false);
   const [viewmovies,setviewmovies]=useState(false);
+  const [viewdashboard,setviewdashboard]=useState(false);
 
-
+const dashboardview=()=>{
+  setaddmovies(false);
+  setviewmovies(false);
+  setviewdashboard(true);
+}
   const addMoviesTab=()=>{
     setaddmovies(true);
     setviewmovies(false);
+    setviewdashboard(false);
   }
   const ViewMoviesTab=()=>{
     setaddmovies(false);
     setviewmovies(true);
+    setviewdashboard(false);
 
   }
   return (
@@ -22,6 +30,7 @@ function AdminHome() {
         <AdminNavbar/>
         <div className='admin-main-container'>
           <div className='admin-sidebar'>
+          <button onClick={dashboardview}>Dashboard</button><br/>
             <button onClick={addMoviesTab}>Add Movies</button><br/>
             <button onClick={ViewMoviesTab}>View Movies</button>
             
@@ -33,6 +42,9 @@ function AdminHome() {
 }
 {
   viewmovies?<DisplayMovies/>:""
+}
+{
+  viewdashboard?<Dashboard/>:""
 }
 
           </div>
